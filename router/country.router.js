17bd3a325445f844efa5d133b68285bc92199bad
DanielAyalaRamirez <daalayalara@ittepic.edu.mx>
2021-05-18 09:05:10 -0600

    Primeros pasos en crud, usando insomnia - v01.1.0

---
+++ router/country.router.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+
+const mongoose = require('mongoose');
+var status = require('http-status');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/countries' , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const Country = require('../models/country.model');
+
+module.exports = () => {
+    router.post('/',(req, res) => {
+        country = req.body;
+        Country.create(country)
+        .then(
+            (data) => {
+                res.json({
+                    code: status.OK,
+                    msg: 'Se inserto correctamente',
+                    data: data
+                })
+            }
+        )
+        .catch(
+            (err) => {
+                res.status(status.BAD_REQUEST)
+                    .json({
+                        code: status.BAD_REQUEST,
+                        msg: 'Ocurrio un error',
+                        err: err.name,
+                        data: err.message
+                    })
+            }
+        )
+        ;
+    });
+
+    return router;
+}
