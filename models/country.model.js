1d5aedde2436e0ac820566400aaa0740074501f2
DanielAyalaRamirez <daalayalara@ittepic.edu.mx>
2021-05-21 00:49:48 -0600

    CRUD - v0.4.0

--- models/country.model.js
+++ models/country.model.js
@@ -1,18 +1,21 @@
-const mongoose = require("mongoose");
+  
+const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  code: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 3,
-  },
-});
+const schema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        code: {
+            type: String,
+            required: true,
+            minLength: 3,
+            maxLength: 3
+        }
+    }
+);
 
-const countryModel = mongoose.model("Country", schema, "country");
+const countryModel = mongoose.model('Country', schema, 'country');
 
 module.exports = countryModel;
--- router/country.router.js
+++ router/country.router.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 var status = require('http-status');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/countries' , {
+mongoose.connect('mongodb://localhost:27017/countries', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -12,31 +12,163 @@ mongoose.connect('mongodb://localhost:27017/countries' , {
 const Country = require('../models/country.model');
 
 module.exports = () => {
-    router.post('/',(req, res) => {
+    /** Insertar países */
+    router.post('/', (req, res) => {
         country = req.body;
+
+        //console.log(req);
+
         Country.create(country)
-        .then(
-            (data) => {
-                res.json({
-                    code: status.OK,
-                    msg: 'Se inserto correctamente',
-                    data: data
-                })
-            }
-        )
-        .catch(
-            (err) => {
-                res.status(status.BAD_REQUEST)
-                    .json({
-                        code: status.BAD_REQUEST,
-                        msg: 'Ocurrio un error',
-                        err: err.name,
-                        data: err.message
-                    })
-            }
-        )
-        ;
+            .then(
+                (data) => {
+                    //console.log(data);
+                    res.json(
+                        {
+                            code: status.OK,
+                            msg: 'Se insertó correctamente',
+                            data: data
+                        }
+                    )
+                    //console.log(res);
+                }
+            )
+            .catch(
+                (err) => {
+                    res.status(status.BAD_REQUEST)
+                        .json(
+                            {
+                                code: status.BAD_REQUEST,
+                                msg: 'Ocurrió un error',
+                                err: err.name,
+                                detal: err.message
+                            }
+                        )
+                }
+            );
     });
+
+    /** Consulta general de países */
+    router.get('/', (req, res) => {
+        Country.find({})
+            .then(
+                (countries) => {
+                    res.json({
+                        code: status.OK,
+                        msg: 'Consulta correcta',
+                        data: countries
+                    })
+                }
+            )
+            .catch(
+                (err) => {
+                    res.status(status.BAD_REQUEST)
+                        .json({
+                            code: status.BAD_REQUEST,
+                            msg: 'Error en la petición',
+                            err: err.name,
+                            detail: err.message
+                        })
+                }
+            )
+    });
+
+    /** Consulta de un país por _id */
+    router.get('/:id', (req, res) => {
+
+        const id = req.params.id;
+
+        Country.findOne({ _id: id })
+            .then(
+                (country) => {
+                    if (country)
+                        res.json({
+                            code: status.OK,
+                            msg: 'Consulta correcta',
+                            data: country
+                        });
+                    else
+                        res.status(status.NOT_FOUND)
+                            .json({
+                                code: status.NOT_FOUND,
+                                msg: 'No se encontró el elemento'
+                            });
+
+                }
+            )
+            .catch(
+                (err) => {
+                    res.status(status.BAD_REQUEST)
+                        .json({
+                            code: status.BAD_REQUEST,
+                            msg: 'Error en la petición',
+                            err: err.name,
+                            detail: err.message
+                        })
+                }
+            )
+    });
+
+    /** Actualización */
+    router.put('/:id', (req, res) => {
+        id = req.params.id;
+        country = req.body;
+        Country.findByIdAndUpdate(id, country, { new: true })
+            .then(
+                (data) => {
+                    console.log(data);
+                    res.json({
+                        code: status.OK,
+                        msg: 'Se actualizó correctamente',
+                        data: data
+                    });
+                }
+            )
+            .catch(
+                (err) => {
+                    console.log(err);
+                    res.status(status.BAD_REQUEST);
+                    res.json({
+                        code: status.BAD_REQUEST,
+                        msg: 'Error en la petición',
+                        err: err.name,
+                        detail: err.message
+                    })
+                }
+            )
+    });
+
+    /** Eliminar */
+    router.delete('/:id', (req, res) => {
+        id = req.params.id;
+        Country.findByIdAndRemove(id)
+            .then(
+                (data) => {
+                    if(data)
+                        res.json({
+                            code: status.OK,
+                            msg: 'Se eliminó correctamente',
+                            data: data
+                        })
+                    else 
+                        res.status(status.NOT_FOUND)
+                        .json({
+                            code: status.NOT_FOUND,
+                            msg: 'No se encontró el elemento'
+                        })
+                }
+            )
+            .catch(
+                (err) => {
+                    res.status(status.BAD_REQUEST)
+                        .json({
+                            code: status.BAD_REQUEST,
+                            msg: 'Error en la petición',
+                            err: err.name,
+                            detail: err.message
+                        })
+                }
+            )
+    })
 
     return router;
 }
